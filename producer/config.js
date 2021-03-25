b368dc4271b10aa087745e06fcbef4dfe11fd6ba
Evan Shortiss <evanshortiss@gmail.com>
2021-03-25 16:44:35 +0000

    chore: change lookup env vars to use sbo format

--- producer/config.js
+++ producer/config.js
@@ -7,7 +7,8 @@ module.exports = {
   HTTP_PORT: get("HTTP_PORT").default(8080).asPortNumber(),
   KAFKA_CLIENT_ID: get('KAFKA_CLIENT_ID').default('order-producer').asString(),
   KAFKA_TOPIC: get('KAFKA_TOPIC').default('orders').asString(),
-  KAFKA_SVC_USERNAME: get('KAFKA_SVC_USERNAME').example('srvc-acct-abc123-xyz').required().asString(),
-  KAFKA_SVC_PASSWORD: get('KAFKA_SVC_PASSWORD').required().asString(),
-  KAFKA_BOOTSTRAP_URL: get('KAFKA_BOOTSTRAP_URL').example('kafka-instance-abcxyz.kafka.devshift.org:443').required().asString()
+
+  KAFKA_SVC_USERNAME: get('KAFKACONNECTION_USER').example('srvc-acct-abc123-xyz').required().asString(),
+  KAFKA_SVC_PASSWORD: get('KAFKACONNECTION_PASSWORD').required().asString(),
+  KAFKA_BOOTSTRAP_URL: get('KAFKACONNECTION_BOOTSTRAPSERVERS').example('kafka-instance-abcxyz.kafka.devshift.org:443').required().asString()
 }
