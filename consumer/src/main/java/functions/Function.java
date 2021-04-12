b4a657fb5247fc0bcf0feb61cd488a61e9a77dc7
Evan Shortiss <evanshortiss@gmail.com>
2021-04-12 19:42:00 +0100

    feat: add thread sleep to quarkus processing

--- consumer/src/main/java/functions/Function.java
+++ consumer/src/main/java/functions/Function.java
@@ -5,9 +5,12 @@ import io.quarkus.funqy.Funq;
 public class Function {
 
     @Funq
-    public Output function(Input input) {
+    public Output function(Input input) throws InterruptedException {
         String msg = "processed bonus for player: " + input.getBy().getUsername();
+
+        Thread.sleep(500);
         System.out.println(msg);
+
         return new Output("processed bonus for player: " + input.getBy().getUsername());
     }
 
